8465af1a3279617e2afdc413fbc2ff5251a14eaf
Júlia Kastrup <jkastrupb@gmail.com>
2020-04-11 00:53:37 -0300

    add test of method that shows booklist

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -15,4 +15,14 @@ public class BibliotecaAppTest {
         BibliotecaApp.showWelcomeMessage();
         assertEquals("Hello! Welcome to Bangalore Public Library!" + "\n",outPut.toString());
     }
+
+    @Test
+    public void testIfShowsBookList() {
+        final ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outPut));
+        BibliotecaApp.showBookList();
+        assertEquals("Title: A menina que roubava livros - Author: Markus Zusak - Year: 2005 - Publisher: Intrínseca\n" +
+                "Title: Moby Dick - Author: Herman Melville - Year: 2008 - Publisher: Cosac Naify\n" +
+                "Title: Cachalote - Author: Daniel Galera - Year: 2010 - Publisher: Quadrinhos na Cia" + "\n",outPut.toString());
+    }
 }
