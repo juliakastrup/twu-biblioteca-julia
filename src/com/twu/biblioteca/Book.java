0267a40f7da766aa6cfb57d712b5d297cd0ea256
Júlia Kastrup <jkastrupb@gmail.com>
2020-04-19 19:26:16 -0300

    creates method to populate book library

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,21 +1,33 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
     String author;
     String title;
-    int year;
-    String publisher;
+    int published_year;
 
-    public Book(String title, String author, int year, String publisher) {
+    public Book(String title, String author, int published_year) {
         this.title = title;
         this.author = author;
-        this.year = year;
-        this.publisher = publisher;
+        this.published_year = published_year;
     }
 
     @Override
     public String toString() {
-        return "Title: "+ title + " - Author: " + author + " - Year: " + year + " - Publisher: " + publisher;
+        return "Title: "+ title + " - Author: " + author + " - Year: " + published_year;
     }
+
+    public static List<Book> populateLibrary() {
+        List bookList = new ArrayList();
+        bookList.add(new Book("A menina que roubava livros","Markus Zusak", 2005));
+        bookList.add(new Book("Moby Dick", "Herman Melville", 2008));
+        bookList.add(new Book("Cachalote", "Daniel Galera", 2010));
+        return bookList;
+    }
+
+
+
 
 }
