9dde16a32c1003d8da7fe94a93b0337100d5ef2d
Júlia Kastrup <jkastrupb@gmail.com>
2020-04-19 19:25:28 -0300

    General refactoring

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -7,21 +7,22 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         BibliotecaApp.showWelcomeMessage();
-        BibliotecaApp.showBookList();
-
+        BibliotecaApp.showInputMenu();
     }
 
     public static void showWelcomeMessage(){
-        String welcomeMessage = "Hello! Welcome to Bangalore Public Library!";
+        String welcomeMessage = "Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!";
         System.out.println(welcomeMessage);
     }
 
-    public static void showBookList(){
-        List bookList = new ArrayList();
-        bookList.add(new Book("A menina que roubava livros","Markus Zusak", 2005, "Intrínseca"));
-        bookList.add(new Book("Moby Dick", "Herman Melville", 2008, "Cosac Naify"));
-        bookList.add(new Book("Cachalote", "Daniel Galera", 2010, "Quadrinhos na Cia"));
-        bookList.forEach((book) -> System.out.println(book.toString()));
+    public static void showInputMenu() {
+        Menu menu = new Menu();
+        menu.show();
     }
+
+
+
+
+
 
 }
