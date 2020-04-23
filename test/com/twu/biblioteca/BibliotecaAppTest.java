0dbe4efa01937198f961f575d09c76d87d866df7
Júlia Kastrup <jkastrupb@gmail.com>
2020-04-23 16:27:55 -0300

    Refactor menu flow to improve user experience

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -8,7 +8,6 @@ import java.io.PrintStream;
 
 public class BibliotecaAppTest {
 
-
     @Test
     public void testIfShowsWelcomeMessage() {
         final ByteArrayOutputStream outPut = new ByteArrayOutputStream();
@@ -18,25 +17,18 @@ public class BibliotecaAppTest {
     }
 
     @Test
-    public void testIfShowsBookList() {
-        Library library = new Library(Library.getDefaultBooks());
-        final ByteArrayOutputStream outPut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outPut));
-        library.show();
-        assertEquals("ID: 1 - Title: A menina que roubava livros - Author: Markus Zusak - Year: 2005\n" +
-                "ID: 2 - Title: Moby Dick - Author: Herman Melville - Year: 2008\n" +
-                "ID: 3 - Title: Cachalote - Author: Daniel Galera - Year: 2010\n",outPut.toString());
-    }
-
-    @Test
     public void testIfShowsMenu() {
         final ByteArrayOutputStream outPut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outPut));
         BibliotecaApp.showInputMenu();
-        assertEquals("Enter the number of your choice:\n" +
-                "1 - List of books\n" +
-                "2 - Checkout book\n" +
-                "0 - Quit\n",outPut.toString());
+        assertEquals("==================M=E=N=U==================\n" +
+        "Enter the number of your choice:\n" +
+        "1 - List of books\n" +
+        "2 - Checkout book\n" +
+        "3 - Return book\n" +
+        "4 - List of movies\n" +
+        "5 - Checkout movie\n" +
+        "0 - Quit\n",outPut.toString());
     }
 
 
